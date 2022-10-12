89e86abd2b8a454548a81b7c81f56e28967f062e
bubuding0809 <bubuding0809@gmail.com>
2022-10-12 12:28:50 +0800

    added fulltext search router

--- src/server/router/city.ts
+++ src/server/router/city.ts
@@ -1,22 +1,43 @@
 import { createRouter } from "./context";
 import { z } from "zod";
 
-export const cityRouter = createRouter().query("getCityByCityName", {
-  input: z
-    .object({
+export const cityRouter = createRouter()
+  .query("getCityByCityName", {
+    input: z.object({
       cityName: z.string(),
-    })
-    .nullish(),
-  resolve({ input, ctx }) {
-    if (!input?.cityName) {
-      return [];
-    }
-    return ctx.prisma.city.findMany({
-      where: {
-        cityName: {
-          contains: input?.cityName,
+    }),
+    async resolve({ input, ctx }) {
+      if (!input?.cityName) {
+        return [];
+      }
+      return await ctx.prisma.city.findMany({
+        where: {
+          cityName: {
+            contains: input?.cityName,
+          },
         },
-      },
-    });
-  },
-});
+      });
+    },
+  })
+  .query("getCityByFullTextSearch", {
+    input: z.object({
+      query: z.string(),
+    }),
+    async resolve({ input, ctx }) {
+      if (!input.query) {
+        return [];
+      }
+      return await ctx.prisma.city.findMany({
+        where: {
+          cityName: {
+            search: input.query,
+          },
+          // Country: {
+          //   countryName: {
+          //     search: input.query,
+          //   },
+          // },
+        },
+      });
+    },
+  });
